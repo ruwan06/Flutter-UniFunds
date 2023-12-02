7a29e06842931064da3020549a13bd1a558ac0fa
Ruwan Wijerathna <ruwanw06@gmail.com>
2023-12-02 23:00:34 +0530

    academicRegPage.dart file created

--- lib/Pages/academicRegPage.dart
+++ lib/Pages/academicRegPage.dart
@@ -3,9 +3,40 @@ import 'package:multi_select_flutter/dialog/multi_select_dialog_field.dart';
 import 'package:uni_funds/Components/background.dart';
 import 'package:multi_select_flutter/multi_select_flutter.dart';
 
-class AcademicReg extends StatelessWidget {
-  const AcademicReg({super.key});
+List<String> selectedValues = [];
 
+extension StringExtension on String {
+  String capitalize() {
+    return "${this[0].toUpperCase()}${this.substring(1)}";
+  }
+}
+
+Map<String, List<String>> data = {
+  "clubs": [
+    "Electronic Club",
+    "Gaming Club",
+    "Gardening Club",
+    "Media Club",
+    "LEO Club"
+  ],
+  "societies": [
+    "Astronomical Society",
+    "Buddhist Society",
+    "Entrepreneurship Society",
+    "Dancing Society",
+    "Computer Society"
+  ],
+  "teams": ["Rangamadale Api Rangakala Ekamuthuwa", "tech"]
+};
+
+class AcademicReg extends StatefulWidget {
+  const AcademicReg({Key? key}) : super(key: key);
+  @override
+  _AcademicReg createState() => _AcademicReg();
+}
+
+class _AcademicReg extends State<AcademicReg> {
+  bool isChecked = false;
   @override
   Widget build(Object context) {
     return MaterialApp(
@@ -14,53 +45,82 @@ class AcademicReg extends StatelessWidget {
         fit: StackFit.expand,
         children: [
           Background(),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              MyMultiSelectDropdown(),
-              SizedBox(
-                height: 30.0,
-              ),
-              MyCheckbox(),
-              ElevatedButton.icon(
-                onPressed: () {
-                  // Add your button click logic here
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.blue,
-                  onPrimary: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0),
+          SingleChildScrollView(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 50.0,
                   ),
-                ),
-                icon: Icon(Icons.card_giftcard), // Icon to the left of the text
-                label: Text('Continue'),
+                  Visibility(
+                    visible: isChecked,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: Colors.black12,
+                      ),
+                      padding: EdgeInsets.only(
+                          bottom: 50.0, left: 10.0, right: 10.0),
+                      child: Column(
+                        children: [
+                          ...data.entries.map((entry) {
+                            return MyMultiSelectDropdown(
+                                entry.key.capitalize(), entry.value);
+                          }),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  MyCheckbox(
+                      isChecked: isChecked, onChecked: _onCheckboxChanged),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      // Add your button click logic here
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                      onPrimary: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                    icon: Icon(
+                        Icons.card_giftcard), // Icon to the left of the text
+                    label: Text('Continue'),
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                ],
               ),
-            ],
+            ),
           )
         ],
       ),
     ));
   }
+
+  void _onCheckboxChanged(bool value) {
+    setState(() {
+      isChecked = value;
+    });
+  }
 }
 
 class MyMultiSelectDropdown extends StatefulWidget {
+  final String? name;
+  final List<String>? options;
+
+  MyMultiSelectDropdown(this.name, this.options);
   @override
   _MyMultiSelectDropdownState createState() => _MyMultiSelectDropdownState();
 }
 
 class _MyMultiSelectDropdownState extends State<MyMultiSelectDropdown> {
-  List<String> selectedValues = [];
-  List<String> options = [
-    'Electronic Club',
-    'Gaming Club',
-    'Media Club',
-    'Rangamadale Api Rangakala Ekamuthuwa',
-    'tech',
-    'Buddhist Society',
-    'Computer Society'
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -73,35 +133,22 @@ class _MyMultiSelectDropdownState extends State<MyMultiSelectDropdown> {
         SizedBox(height: 30),
         Container(
           child: MultiSelectDialogField<String>(
-            items: options.map((e) => MultiSelectItem<String>(e, e)).toList(),
+            items: widget.options!
+                .map((e) => MultiSelectItem<String>(e, e))
+                .toList(),
             initialValue: selectedValues,
-            title: Text("Select Options"),
+            title: Text("Select ${widget.name ?? ""}"),
             selectedColor: Colors.green,
             unselectedColor: Colors.grey[300],
             buttonIcon: Icon(Icons.keyboard_arrow_down),
-            buttonText: Text("Select options"),
+            buttonText: Text("Select ${widget.name ?? ""}"),
             onConfirm: (values) {
               if (values == null) return;
               setState(() {
-                selectedValues = values;
-              });
-            },
-          ),
-          width: 300.0,
-        ),
-        Container(
-          child: MultiSelectDialogField<String>(
-            items: options.map((e) => MultiSelectItem<String>(e, e)).toList(),
-            initialValue: selectedValues,
-            title: Text("Select Options"),
-            selectedColor: Colors.green,
-            unselectedColor: Colors.grey[300],
-            buttonIcon: Icon(Icons.keyboard_arrow_down),
-            buttonText: Text("Select options"),
-            onConfirm: (values) {
-              if (values == null) return;
-              setState(() {
-                selectedValues = values;
+                selectedValues
+                    .removeWhere((item) => widget.options!.contains(item));
+                selectedValues.addAll(values);
+                print("values => $selectedValues");
               });
             },
           ),
@@ -113,27 +160,34 @@ class _MyMultiSelectDropdownState extends State<MyMultiSelectDropdown> {
 }
 
 class MyCheckbox extends StatefulWidget {
+  final bool isChecked;
+  final ValueChanged<bool> onChecked;
+
+  const MyCheckbox({Key? key, required this.isChecked, required this.onChecked})
+      : super(key: key);
   @override
   _MyCheckboxState createState() => _MyCheckboxState();
 }
 
 class _MyCheckboxState extends State<MyCheckbox> {
-  bool isChecked = false;
-
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Checkbox(
-          value: isChecked,
-          onChanged: (bool? value) {
-            setState(() {
-              isChecked = value!;
-            });
+          value: widget.isChecked,
+          onChanged: (value) {
+            widget.onChecked(value ?? false);
           },
         ),
-        Text('Accept Terms and Conditions'),
+        Container(
+          width: 250.0,
+          child: Text(
+            'Do you hold a position in a society or any organization, such as a club?',
+            textAlign: TextAlign.center,
+          ),
+        ),
       ],
     );
   }
